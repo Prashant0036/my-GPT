efe715c2ca14f7a68061e85ab331f7e3437013cb
Prashant Saraswat <prashantsaraswat036@gmail.com>
2024-12-06 21:22:47 +0530

    Update llm_model.py
    Changed the model

--- llm_model.py
+++ llm_model.py
@@ -3,7 +3,7 @@ from langchain_groq import ChatGroq
 import os
 load_dotenv()
 
-llm = ChatGroq(groq_api_key=os.getenv('GROQ_API_KEY'),model_name="llama-3.2-90b-text-preview")
+llm = ChatGroq(groq_api_key=os.getenv('GROQ_API_KEY'),model_name="llama3-8b-8192")
 
 if __name__ == "__main__":
     response = llm.invoke("Who is Lord Krishna")
